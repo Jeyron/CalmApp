373d2d3fe4f0719c80a2fa4771b921ab2dedeb0f
Jeyron <31875131+Jeyron@users.noreply.github.com>
2017-11-20 17:34:40 -0600

    Segundo proyecto 3

--- app/src/main/java/com/example/jeiro/calmapp/MainActivity.java
+++ app/src/main/java/com/example/jeiro/calmapp/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.jeiro.calmapp;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import  static android.hardware.Sensor.TYPE_LIGHT;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+    private SensorEventListener lightEventListener;
+    private View root;
+    private float maxValue;
+
+    Button btn_inicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        btn_inicio = (Button) findViewById(R.id.btn_inicio);
+        btn_inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Navegacion.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
