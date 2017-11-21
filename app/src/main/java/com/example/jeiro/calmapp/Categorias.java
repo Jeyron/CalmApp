74f5f79743e69c6e5aef7c165251ca7f3806f78f
Ronald Alfonso Rodriguez Centeno <rcrodriguez@estudiantec.cr>
2017-11-20 21:33:21 -0600

    Categorias funcional

--- app/src/main/java/com/example/jeiro/calmapp/Categorias.java
+++ app/src/main/java/com/example/jeiro/calmapp/Categorias.java
@@ -7,6 +7,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.jeiro.calmapp.Datos.datos_categoria;
 import com.example.jeiro.calmapp.Datos.datos_contenido;
@@ -44,6 +45,9 @@ public class Categorias extends AppCompatActivity
         btn_eliminar_icono = (Button) findViewById(R.id.btn_eliminar_icono);
         btn_guardar_icono = (Button) findViewById(R.id.btn_guardar_icono);
 
+        spn_lista_iconos = (Spinner) findViewById(R.id.spn_lista_iconos);
+        spn_lista_categorias = (Spinner) findViewById(R.id.spn_lista_categorias);
+
         spn_lista_categorias.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
         {
             @Override public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long arg3)
@@ -59,25 +63,39 @@ public class Categorias extends AppCompatActivity
 
     private void cargar_spinner_categorias ()
     {
-        datos_categoria datos_categoria = new datos_categoria();
-        ArrayList<entidad_categoria> lista_categorias = datos_categoria.obtener_categorias(this);
-        String array_categorias[] = new String[lista_categorias.size()];
-        for (int i = 0; i < lista_categorias.size(); i++) array_categorias[i] = lista_categorias.get(i).getDescripcion();
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, array_categorias);
-        spn_lista_categorias.setAdapter(adapter);
+        try
+        {
+            datos_categoria datos_categoria = new datos_categoria();
+            ArrayList<entidad_categoria> lista_categorias = datos_categoria.obtener_categorias(this);
+            String array_categorias[] = new String[lista_categorias.size()];
+            for (int i = 0; i < lista_categorias.size(); i++) array_categorias[i] = lista_categorias.get(i).getDescripcion();
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, array_categorias);
+            spn_lista_categorias.setAdapter(adapter);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, "Error " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void cargar_spinner_iconos ()
     {
-        datos_categoria datos_categoria = new datos_categoria();
-        String selected_item = spn_lista_categorias.getSelectedItem().toString();
-        entidad_categoria entidad_categoria = datos_categoria.obtener_categoria(this, selected_item);
+        try {
+            datos_categoria datos_categoria = new datos_categoria();
+            String selected_item = spn_lista_categorias.getSelectedItem().toString();
+            entidad_categoria entidad_categoria = datos_categoria.obtener_categoria(this, selected_item);
 
-        datos_contenido datos_iconos = new datos_contenido();
-        ArrayList<entidad_contenido> lista_contenidos = datos_iconos.obtener_contenido_por_categoria(this, entidad_categoria);
-        String array_contenidos[] = new String[lista_contenidos.size()];
-        for (int i = 0; i < lista_contenidos.size(); i++) array_contenidos[i] = lista_contenidos.get(i).getNombre();
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, array_contenidos);
-        spn_lista_categorias.setAdapter(adapter);
+            datos_contenido datos_iconos = new datos_contenido();
+            ArrayList<entidad_contenido> lista_contenidos = datos_iconos.obtener_contenido_por_categoria(this, entidad_categoria);
+            String array_contenidos[] = new String[lista_contenidos.size()];
+            for (int i = 0; i < lista_contenidos.size(); i++)
+                array_contenidos[i] = lista_contenidos.get(i).getNombre();
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, array_contenidos);
+            spn_lista_iconos.setAdapter(adapter);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, "Error " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
